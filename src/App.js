ed707b185809d1cd890f53406f37dc255405600c
Hari-Venkatesh-P <harivenkat1110@gmail.com>
2021-03-21 11:17:13 +0530

    Base setup with ReactGA?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import ReactGA from 'react-ga';
 import LoginScreen from "../src/screens/Login"
 import HomeScreen from "../src/screens/Home"
 import PageNotFound from "../src/screens/PageNotFound"
 
+
+ReactGA.initialize('UA-192616362-1');
+
 function App() {
 
+  useEffect (()=>{
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  })
   return (
     <div className="App">
       <BrowserRouter>
